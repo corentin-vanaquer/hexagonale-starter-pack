95ce2b61555aa09dcfe638ad48bf7419741812b2
cva23610 <corentin.vanaquer@open-groupe.com>
2025-01-09 14:25:09 +0100

    feat(application): structure

---
+++ application/src/main/java/com/example/application/business/MyServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.application.business;
+
+import com.example.domain.ports.driving.MyDrivingPort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyServiceImpl implements MyDrivingPort {
+}
