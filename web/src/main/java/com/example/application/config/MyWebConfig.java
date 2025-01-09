5174aec002fc65ab53a175e312f90b9dcbd7b7ec
cva23610 <corentin.vanaquer@open-groupe.com>
2025-01-09 15:27:31 +0100

    core(build): minor fix
    - Name conflict
    - H2 config

--- web/src/main/java/com/example/application/config/MyWebConfig.java
+++ web/src/main/java/com/example/application/config/MyWebConfig.java
@@ -1,7 +1,7 @@
-package com.example.infrastructure.config;
+package com.example.application.config;
 
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MyConfig {
+public class MyWebConfig {
 }
