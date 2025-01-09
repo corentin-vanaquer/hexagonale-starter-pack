eb1c73bd526cf85c09f13aa417afac05627e78a9
cva23610 <corentin.vanaquer@open-groupe.com>
2025-01-09 11:27:46 +0100

    feat(domain): structure

---
+++ domain/src/main/java/com/example/domain/model/MyRecord.java
@@ -0,0 +1,7 @@
+package com.example.domain.model;
+
+import lombok.Builder;
+
+@Builder(toBuilder = true)
+public record MyRecord() {
+}
---
+++ domain/src/main/java/com/example/domain/ports/driving/MyDrivingPort.java
@@ -0,0 +1,4 @@
+package com.example.domain.ports.driving;
+
+public interface MyDrivingPort {
+}
